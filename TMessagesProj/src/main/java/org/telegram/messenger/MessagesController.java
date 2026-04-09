0389f629ad0f3e1876efc0bba42de6c32876bcf4
Timothy Redaelli <timothy@fsfe.org>
2026-04-09 20:49:06 +0200

    [MG] Message Details menu and debug toggles

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -1565,6 +1565,9 @@ public class MessagesController extends BaseController implements NotificationCe
         keepAliveService = mainPreferences.getBoolean("keepAliveService", false);
         backgroundConnection = mainPreferences.getBoolean("keepAliveService", false);
         promoDialogId = mainPreferences.getLong("proxy_dialog", 0);
+        if (SharedConfig.removeAdsAndProxySponsor) {
+            promoDialogId = 0;
+        }
         nextPromoInfoCheckTime = mainPreferences.getInt("nextPromoInfoCheckTime", 0);
         promoDialogType = mainPreferences.getInt("promo_dialog_type", 0);
         promoPsaMessage = mainPreferences.getString("promo_psa_message", null);
@@ -10573,6 +10576,9 @@ public class MessagesController extends BaseController implements NotificationCe
     private long lastCheckPromoInfoTime;
 
     private void checkPromoInfoInternal(boolean reset) {
+        if (SharedConfig.removeAdsAndProxySponsor) {
+            return;
+        }
         if (reset && checkingPromoInfo) {
             checkingPromoInfo = false;
         }
@@ -20950,6 +20956,9 @@ public class MessagesController extends BaseController implements NotificationCe
     }
 
     public SponsoredMessagesInfo getSponsoredMessages(long dialogId) {
+        if (SharedConfig.removeAdsAndProxySponsor) {
+            return null;
+        }
         SponsoredMessagesInfo info = sponsoredMessages.get(dialogId);
         if (info != null && (info.loading || Math.abs(SystemClock.elapsedRealtime() - info.loadTime) <= 5 * 60 * 1000)) {
             return info;
@@ -23408,6 +23417,7 @@ public class MessagesController extends BaseController implements NotificationCe
     }
 
     public boolean isSponsoredDisabled() {
+        if (SharedConfig.removeAdsAndProxySponsor) return true;
         TLRPC.UserFull userFull = getUserFull(getUserConfig().getClientUserId());
         if (userFull == null) return false;
         return !userFull.sponsored_enabled;
