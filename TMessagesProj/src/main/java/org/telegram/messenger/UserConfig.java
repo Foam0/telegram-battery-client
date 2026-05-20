8017a3e2ec294c86c70fb86f4299d96f20e6d6fa
Timothy Redaelli <timothy@fsfe.org>
2026-05-20 21:02:56 +0200

    [MG] use rear camera for round video messages

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -62,6 +62,7 @@ public class UserConfig extends BaseController {
     public boolean suggestContacts = true;
     public boolean showCallsTab;
     public boolean sendLargePhotos = false;
+    public boolean rearRoundCamera = false;
     public boolean hasSecureData;
     public int loginTime;
     public TLRPC.TL_help_termsOfService unacceptedTermsOfService;
@@ -162,6 +163,7 @@ public class UserConfig extends BaseController {
                     editor.putBoolean("showCallsTab", showCallsTab);
                     editor.putBoolean("suggestContacts", suggestContacts);
                     editor.putBoolean("sendLargePhotos", sendLargePhotos);
+                    editor.putBoolean("rearRoundCamera", rearRoundCamera);
                     editor.putBoolean("hasSecureData", hasSecureData);
                     editor.putBoolean("notificationsSettingsLoaded4", notificationsSettingsLoaded);
                     editor.putBoolean("notificationsSignUpSettingsLoaded", notificationsSignUpSettingsLoaded);
@@ -314,6 +316,7 @@ public class UserConfig extends BaseController {
             showCallsTab = preferences.getBoolean("showCallsTab", false);
             suggestContacts = preferences.getBoolean("suggestContacts", true);
             sendLargePhotos = preferences.getBoolean("sendLargePhotos", false);
+            rearRoundCamera = preferences.getBoolean("rearRoundCamera", false);
             hasSecureData = preferences.getBoolean("hasSecureData", false);
             notificationsSettingsLoaded = preferences.getBoolean("notificationsSettingsLoaded4", false);
             notificationsSignUpSettingsLoaded = preferences.getBoolean("notificationsSignUpSettingsLoaded", false);
@@ -486,6 +489,7 @@ public class UserConfig extends BaseController {
         showCallsTab = false;
         suggestContacts = true;
         sendLargePhotos = false;
+        rearRoundCamera = false;
         unreadDialogsLoaded = true;
         hasValidDialogLoadIds = true;
         unacceptedTermsOfService = null;
